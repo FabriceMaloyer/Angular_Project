775e6a88e06d3e816dc5877f15eff943c756637f
FabriceMaloyer <fabrice.maloyer@viacesi.fr>
2024-04-17 11:54:14 +0200

    un peu de css et un helper qui sera peut etre utile

--- src/app/Calendar/Calendar-helper.ts
+++ src/app/Calendar/Calendar-helper.ts
@@ -20,3 +20,26 @@ function generateWorkdays(year: number): Date[] {
 // Exemple pour l'année 2023
 const workdays2023 = generateWorkdays(2023);
 console.log(workdays2023);
+
+
+
+function generateWorkweeks(year: number): Date[][] {
+  const workweeks: Date[][] = [];
+  const startDate = new Date(year, 0, 1); // 1er janvier
+  const endDate = new Date(year, 11, 31); // 31 décembre
+
+  const oneWeek = 7 * 24 * 60 * 60 * 1000; // Durée d'une semaine en millisecondes
+
+  let currentWeekStart = startDate;
+  while (currentWeekStart <= endDate) {
+    const currentWeekEnd = new Date(currentWeekStart.getTime() + oneWeek);
+    workweeks.push([new Date(currentWeekStart), new Date(currentWeekEnd)]);
+    currentWeekStart = new Date(currentWeekEnd.getTime() + oneWeek);
+  }
+
+  return workweeks;
+}
+
+// Exemple pour l'année 2023
+const workweeks2023 = generateWorkweeks(2023);
+console.log(workweeks2023);
